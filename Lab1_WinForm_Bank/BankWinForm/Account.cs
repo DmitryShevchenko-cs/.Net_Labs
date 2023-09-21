6074b729f146cb9bc7e119c79ae05853438f04e7
Dmitry <dima.shevchenko10@gmail.com>
2023-09-21 17:22:28 +0300

    Static account class for remember user

---
+++ Lab1_WinForm_Bank/BankWinForm/Account.cs
@@ -0,0 +1,15 @@
+﻿using BankLibrary;
+using BankLibrary.Services;
+
+namespace BankWinForm
+{
+    static class Account
+    {
+        public static User? user { get; set; }
+
+        public static void Login(User user) => Account.user = user;
+
+        public static void Logout() => user = null;
+
+    }
+}
