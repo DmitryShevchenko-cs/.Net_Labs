5a334d165924fc31ed7d2d88dc3a4cfccce84695
Dmitry <dima.shevchenko10@gmail.com>
2023-10-23 15:26:08 +0300

    lab2

---
+++ Lab2_Linq/Lab2_LINQ/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab2_Linq
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            String[] A = { "Falluot 3", "Daxter 2", "System Shok 2", "Morrowind", "Pes 2013" }; 
+            int[] B = { 2, -7, -10, 6, 7, 9, 3 }; 
+            String[] C = { "Light Green", "Red", "Green", "Yellow", "Purple", 
+                "Dark Green", "Light Red", "Dark Red", "Dark Yellow", "Light Yellow" };
+            
+            List<string> myCars = new List<String> { "Yugo", "Aztec", "BMW" }; 
+            List<string> yourCars = new List<String> { "BMW", "Saab", "Aztec" };
+            
+            //1
+            Console.WriteLine("\ntask1");
+            var task1 = A.Where(i => i.Contains(' '));
+            foreach (var a in task1) 
+                Console.Write($"{a}, ");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            //2
+            Console.WriteLine("\ntask2");
+            var task2 = B.Where(i => i > 0);
+            foreach (var b in task2)
+                Console.Write($"{b}, ");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            //3
+            Console.WriteLine("\ntask3");
+            var task3 = C.Where(i => i.Contains("Red"));
+            foreach (var c in task3)
+                Console.Write($"{c}, ");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            //4
+            Console.WriteLine("\ntask4");
+            var rand = new Random();
+            var cars = new List<Car>();
+            var names = myCars.Concat(yourCars).ToList();
+            for (int i = 0; i < 6; i++)
+            {
+                cars.Add(new Car
+                {
+                    MaxSpeed = rand.Next(150, 301),
+                    Color = "black",
+                    Name = names[i],
+                    NumberOfPlaces = 5
+                });
+            }
+
+            var CarDpeenOver200 = cars.Where(i => i.MaxSpeed > 200);
+            foreach (var car in CarDpeenOver200)
+            {
+                Console.WriteLine($"{car.Name}, {car.MaxSpeed}, {car.Color}, {car.NumberOfPlaces}");
+            }
+            Console.WriteLine();
+            
+            //5
+            Console.WriteLine("\ntask5");
+            var products = new List<Product>();
+            for (int i = 0; i < 6; i++)
+            {
+                var name = rand.Next(100, 1000);
+                var amount = rand.Next(1, 10);
+                products.Add(new Product
+                {
+                    Name = name.ToString(),
+                    Amount = amount,
+                    Description = $"{name} in amount of {amount}"
+                });
+            }
+
+            var productsLINQ = products.Where(i => i.Amount < 5);
+            foreach (var prod in productsLINQ)
+            {
+                Console.WriteLine($"{prod.Description}");
+            }
+            Console.WriteLine();
+
+            //6
+            Console.WriteLine("\ntask6");
+            var task6 = myCars.Concat(yourCars).ToList();
+            foreach (var c in task6)
+                Console.Write($"{c}, ");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+    class Car
+    {
+        public int MaxSpeed { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public int NumberOfPlaces { get; set; }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Lab2_Linq/Project.Tests/UnitTest1.cs
@@ -0,0 +1,90 @@
+using System.Linq;
+using NUnit.Framework;
+
+
+using global::Project.Extensions;
+using Project.ExtendedDictionary;
+using Project.Extensions;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ReverseTest_StringReversed_OK()
+    {
+        string str = "Some little test string";
+        string reversedStr = "gnirts tset elttil emoS";
+        Assert.That(reversedStr.Equals(str.Reverse()));
+        str = "qwerty";
+        reversedStr = "ytrewq";
+        Assert.That(reversedStr.Equals(str.Reverse()));
+    }
+    
+    [Test]
+    public void CountElementOfSting_returnRightNumber()
+    {
+        string str = "Some little test string";
+        Assert.That(str.CountElement('i') == 2);
+        Assert.That(str.CountElement('e') == 3);
+        Assert.That(str.CountElement('t') == 5);
+    }
+    
+    [Test]
+    public void CountElementOfArray_returnRightNumber()
+    {
+        int[] arr = { 1, 2, 2, 3, 4, 2, 2 };
+        Assert.That(arr.CountElement(2) == 4);
+
+        char[] arr2 = { 'a', 'b', 'c', 'd', 'c', 'c', 'e', 'c' };
+        Assert.That(arr2.CountElement('c') == 4);
+    }
+    
+    [Test]
+    public void RemoveRepeatedElements_removedOk()
+    {
+        int[] arr = { 1, 2, 2, 3, 4, 2, 2 };
+        Assert.That(arr.MakeUnique().Count(s => s == 1) == 1);
+        Assert.That(arr.MakeUnique().Count(s => s == 2) == 1);
+        Assert.That(arr.MakeUnique().Count(s => s == 3) == 1);
+        Assert.That(arr.MakeUnique().Count(s => s == 4) == 1);
+
+        char[] arr2 = { 'a', 'b', 'c', 'd', 'c', 'c', 'e', 'c' };
+        Assert.That(arr2.MakeUnique().Count(s => s == 'a') == 1);
+        Assert.That(arr2.MakeUnique().Count(s => s == 'b') == 1);
+        Assert.That(arr2.MakeUnique().Count(s => s == 'c') == 1);
+        Assert.That(arr2.MakeUnique().Count(s => s == 'd') == 1);
+        Assert.That(arr2.MakeUnique().Count(s => s == 'e') == 1);
+    }
+
+    [Test]
+    public void AddInDictionaryTest_OK()
+    {
+        ExtendedDictionary<int, string, double> dictionary = new ExtendedDictionary<int, string, double>();
+        dictionary.Add(1, "1", 10);
+        
+        Assert.That(dictionary.Count == 1);
+        Assert.That(dictionary.ContainsKey(1));
+        Assert.That(dictionary.ContainsKey(2) is false);
+        Assert.That(dictionary.ContainsValues("1", 10));
+        
+        dictionary.Add(2, "2", 20);
+        Assert.That(dictionary.Count == 2);
+        Assert.That(dictionary.ContainsKey(2));
+
+        int i = 1;
+        int j = 10;
+        foreach (var element in dictionary)
+        {
+            Assert.That(element.Value1 == i.ToString());
+            Assert.That(element.Value2 == j);
+            i++;
+            j += 10;
+        }
+        
+    }
+    
+}
